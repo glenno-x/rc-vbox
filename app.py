87ef7cb57d422e1e7db8082e302fd9aa56c43e82
Glenn Mazengarb <gmazengarb@gmail.com>
2021-08-28 14:19:30 +1000

    Use ConfigParser

--- app.py
+++ app.py
@@ -2,19 +2,21 @@ from PyQt5.QtWidgets import (
     QApplication, QDialog, QMainWindow, QMessageBox
 )
 import subprocess
-# import sys
-
+from configparser import ConfigParser
 # Only needed for access to command line arguments
 # import sys
-
 from main_window import Ui_MainWindow
 from connection_dialog import Ui_ConnectionDialog
 
+CONFIG_FILE = 'rc-vbox.ini'
+
 
 class ConnectionDialog(QDialog, Ui_ConnectionDialog):
-    def __init__(self, parent):
+    def __init__(self, parent, default_user, default_machine):
         super().__init__(parent)
         self.setupUi(self)
+        self.lineEdit_user.setPlaceholderText(default_user)
+        self.lineEdit_machine.setPlaceholderText(default_machine)
 
 
 class AppMainWindow(QMainWindow, Ui_MainWindow):
@@ -44,13 +46,37 @@ class AppMainWindow(QMainWindow, Ui_MainWindow):
 
     def connection_dialog(self):
         r_args = []
-        dialog = ConnectionDialog(self)
+        config = ConfigParser()
+
+        # Detect and load config.ini
+        try:
+            with open(CONFIG_FILE) as file:
+                config.read(file)
+        except FileNotFoundError:
+            pass
+        if not ('connection' in config):
+            config['connection'] = {'user': '', 'machine': ''}
+        sub_config = config['connection']
+        config_user = sub_config['user']
+        config_machine = sub_config['machine']
+
+        dialog = ConnectionDialog(self, config_user, config_machine)
         if dialog.exec():
             if dialog.lineEdit_user.text() and dialog.lineEdit_machine.text():
                 self.cmd_prefix = dialog.lineEdit_user.text() + '@' + dialog.lineEdit_machine.text()
             else:
                 self.cmd_prefix = None
-            # TODO:  Save the user/server pair as config
+
+            # Save the connection config
+            config['connection']['user'] = dialog.lineEdit_user.text()
+            config['connection']['machine'] = dialog.lineEdit_machine.text()
+            try:
+                with open(CONFIG_FILE, 'wt') as file:
+                    config.write(file)
+            except FileNotFoundError:
+                pass
+
+            # Launch the vbox command
             if self.cmd_prefix:
                 r_args.extend(['ssh', self.cmd_prefix])
             r_args.extend(['VBoxManage', 'list', 'vms'])
